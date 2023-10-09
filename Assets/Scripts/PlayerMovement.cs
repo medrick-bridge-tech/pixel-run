4e800200837d52aa5672c75e269d3df6df826a5a
Codidodido <codidodido@gmail.com>
2023-10-09 15:27:41 +0330

    Modify player movement
    Lose game when colliding to enemy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -12,25 +13,30 @@ public class PlayerMovement : MonoBehaviour
     private float _xVector;
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
+    private CinemachineVirtualCamera _cvm;
     
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _cvm = GetComponentInChildren<CinemachineVirtualCamera>();
         _animator = GetComponent<Animator>();
     }
-    
+
     private void Update()
     {
         Run();
         Jump();
         HandleAnimation();
     }
-    
+
     private void Run()
     {
-        _xVector = Input.GetAxisRaw("Horizontal");
-        transform.position += (Vector3)new Vector2(_xVector * moveSpeed * Time.deltaTime, 0);
-        Flip();
+        if (_animator.GetBool("IsAlive"))
+        {
+            _xVector = Input.GetAxisRaw("Horizontal");
+            transform.position += (Vector3)new Vector2(_xVector * moveSpeed * Time.deltaTime, 0);
+            Flip();
+        }
     }
 
     private void Jump()
@@ -45,7 +51,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_xVector != 0)
         {
-            transform.localScale = (Vector3)new Vector2(_xVector, 1);    
+            transform.localScale = (Vector3)new Vector2(_xVector, 1);
         }
     }
 
@@ -53,28 +59,28 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_xVector != 0 && !_animator.GetBool("IsJump"))
         {
-            _animator.SetFloat("Speed",1);
+            _animator.SetFloat("Speed", 1);
         }
         else
         {
-            _animator.SetFloat("Speed",0);
+            _animator.SetFloat("Speed", 0);
         }
 
         if (!CheckDistanceFromEarth())
         {
-            _animator.SetBool("IsJump",true);
+            _animator.SetBool("IsJump", true);
         }
         else
         {
-            _animator.SetBool("IsJump",false);
+            _animator.SetBool("IsJump", false);
         }
-        
+
     }
 
     private bool CheckDistanceFromEarth()
     {
         RaycastHit2D hit;
-        hit = Physics2D.Raycast(transform.position, -transform.up,_distanceFromEarth,LayerMask.GetMask("Ground"));
+        hit = Physics2D.Raycast(transform.position, -transform.up, _distanceFromEarth, LayerMask.GetMask("Ground"));
         if (hit.collider != null)
         {
             return true;
@@ -82,6 +88,20 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             return false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("do");
+            _animator.SetBool("IsAlive",false);
+            Destroy(GetComponent<BoxCollider2D>());
+            _rigidbody2D.AddForce(new Vector2(0,jumpSpeed),ForceMode2D.Impulse);
+            GameObject crow = new GameObject();
+            crow.transform.position = transform.position;
+            _cvm.Follow = crow.transform;
         }
     }
 }
