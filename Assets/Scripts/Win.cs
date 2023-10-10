ffef3e9977a8456b81ff157f5aacd2c2cdc4f2e2
Codidodido <codidodido@gmail.com>
2023-10-10 23:27:02 +0330

    Modify win script and edit race flag in scene

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -4,16 +4,22 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Win : MonoBehaviour
 {
     [SerializeField] private List<GameObject> winners = new List<GameObject>();
+    [SerializeField] private GameObject winUI;
+    [SerializeField] private AudioClip audioClip;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             winners.Add(other.gameObject);
             Destroy(other.gameObject.GetComponent<PlayerMovement>());
-            other.GetComponent<Animator>().SetFloat("Speed",0);
+            other.GetComponent<Animator>().SetFloat("Speed", 0);
+            winUI.SetActive(true);
+            GetComponent<AudioSource>().PlayOneShot(audioClip);
         }
     }
 }
