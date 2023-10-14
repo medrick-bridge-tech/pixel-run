5f2be94420b85b205f551c60a3f502a683007267
Codidodido <codidodido@gmail.com>
2023-10-14 08:05:19 +0330

    Create skin card controller script
    Create skin card controller script which handles card view and skin selection

---
+++ Assets/Scripts/SkinCardController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinCardController : MonoBehaviour
+{
+    [SerializeField] private Image cardSkin;
+    [SerializeField] private TextMeshProUGUI cardText;
+
+    public void SetCard(Sprite skin, string name)
+    {
+        cardSkin.sprite = skin;
+        cardText.text = name;
+    }
+
+    public void SelectSkin()
+    {
+        PlayerPrefs.SetString("skin",cardSkin.sprite.name);
+        Debug.Log(cardSkin.sprite.name);
+    }
+}
