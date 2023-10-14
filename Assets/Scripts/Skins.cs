e9cb98bec0e90d00e58351ecbe1dd3ce06042b45
Codidodido <codidodido@gmail.com>
2023-10-14 08:00:42 +0330

    Create skin card class and skins scriptable object

---
+++ Assets/Scripts/Skins.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SkinCard{
+    public string name;
+    public int price;
+    public Sprite skinSprite;
+}
+
+[CreateAssetMenu(fileName = "Skin", menuName = "Skin")]
+public class Skins : ScriptableObject
+{
+    [SerializeField] private List<SkinCard> skinCards = new List<SkinCard>();
+
+    public List<SkinCard> SkinCards => skinCards;
+}
