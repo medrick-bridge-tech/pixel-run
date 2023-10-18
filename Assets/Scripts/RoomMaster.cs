84512fcf9f86142b5113484a2ff5d3873f39cd6a
Codidodido <codidodido@gmail.com>
2023-10-18 19:16:58 +0330

    Change room capacity by drop down

--- Assets/Scripts/RoomMaster.cs
+++ Assets/Scripts/RoomMaster.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
@@ -6,13 +7,16 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class RoomMaster : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private int roomCapacity = 2;
+
     public void HostRoom()
     {
         var randomID = Random.Range(1000000, 9999999);
-        PhotonNetwork.CreateRoom(randomID.ToString(),new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
+        PhotonNetwork.CreateRoom(randomID.ToString(),new RoomOptions { MaxPlayers = roomCapacity}, TypedLobby.Default);
         Debug.Log($"Room Created{randomID}");
     }
 
@@ -25,5 +29,12 @@ public class RoomMaster : MonoBehaviourPunCallbacks
     {
         LoadingData.SceneToLoad = "WaitingRoom";
         SceneManager.LoadScene("Loading");
+    }
+
+    public void ChangeRoomCapacity(TMP_Dropdown dropDown)
+    {
+        var selectedOption = dropDown.value;
+        roomCapacity = Convert.ToInt32(dropDown.options[selectedOption].text);
+        Debug.Log(roomCapacity);
     }
 }
