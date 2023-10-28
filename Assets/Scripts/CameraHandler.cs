776b29fa7f8904cb959fdbff11c26cb1e5f195e1
Codidodido <codidodido@gmail.com>
2023-10-28 16:54:38 +0330

    Refactor camera handler script
    Comply single responsibility of camera handler script

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -11,51 +11,24 @@ public class CameraHandler : MonoBehaviour
 {
     private CinemachineVirtualCamera _virtualCamera;
 
-    public const Byte OnLoseAllPlayers = 1;
-
-    private void OnEnable()
-    {
-        _virtualCamera.Follow.GetComponent<PlayerLose>().onLoseGame += FindNewTarget;
-    }
-    
-    private void OnDisable()
-    {
-        _virtualCamera.Follow.GetComponent<PlayerLose>().onLoseGame -= FindNewTarget;
-    }
-    
     private void Awake()
     {
         _virtualCamera = GetComponent<CinemachineVirtualCamera>();
     }
 
-    private void FindNewTarget()
+    public void FindNewTarget()
     {
         var player = PhotonNetwork.LocalPlayer.GetNext();
-        if (player!=null)
+        if (player != null)
         {
             var viewID = player.ActorNumber;
             viewID = (viewID * 1000) + 1;
-            Debug.Log(viewID);
             PhotonView photonView = PhotonNetwork.GetPhotonView(viewID);
 
             if (photonView != null)
             {
                 _virtualCamera.Follow = photonView.transform;
             }
-            else
-            {
-                SendGameOverEvent();
-            }
         }
-        else
-        {
-            SendGameOverEvent();
-        }
-    }
-
-    private void SendGameOverEvent()
-    {
-        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All };
-        PhotonNetwork.RaiseEvent(OnLoseAllPlayers, null, raiseEventOptions, SendOptions.SendReliable);
     }
 }
