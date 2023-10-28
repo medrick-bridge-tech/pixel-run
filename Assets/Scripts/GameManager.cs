333337ccabdb690ccd7bd96a4e2ea454ea1f9501
Codidodido <codidodido@gmail.com>
2023-10-28 17:05:09 +0330

    Modify race win handler & game manager scripts
    - Register players into player list
    - Remove player from list when lose game
    - Add handle race method to handle lose and win
    - Write get players count method to gets alive players count

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,16 +7,27 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class GameManager : MonoBehaviourPun, IOnEventCallback
+public class GameManager : MonoBehaviourPun
 {
     [SerializeField] private Transform startPosition;
     [SerializeField] private PositionMapper positionMapper;
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
     [SerializeField] private PhotonCountdown photonCountdown;
     [SerializeField] private Canvas loseUICanvas;
-
+    [SerializeField] private Canvas winUICanvas;
+    [SerializeField] private RaceWinHandler winHandler;
+    
     private GameObject _player;
+    private PhotonView _photonView;
+    private bool _isGameStart;
+    
+    
+    [SerializeField] List<PhotonView> _playersList = new List<PhotonView>();
+    public List<PhotonView> PlayersList => _playersList;
 
     private void OnEnable()
     {
@@ -26,34 +37,82 @@ public class GameManager : MonoBehaviourPun, IOnEventCallback
 
     private void OnDisable()
     {
-        photonCountdown.onRaceStart -= StartRace;
         PhotonNetwork.RemoveCallbackTarget(this);
-    }
-
-    public void OnEvent(EventData eventData)
-    {
-        byte eventCode = eventData.Code;
-        if (eventCode == 1)
-        {
-            DisplayGameOver();
-        }
     }
 
     private void Awake()
     {
         _player = PhotonNetwork.Instantiate("Player", startPosition.position, Quaternion.identity);
+        _player.GetComponent<PlayerLose>().onLoseGame += RemovePlayer;
         positionMapper.SetTarget(_player);
         positionMapper.UpdateGraphics(_player.GetComponent<SpriteRenderer>().sprite);
         virtualCamera.Follow = _player.transform;
     }
 
+    private void Start()
+    {
+        _isGameStart = false;
+    }
+
+    private void Update()
+    {
+        if (_isGameStart)
+        {
+            HandleRace();
+        }
+    }
+    
     private void StartRace()
     {
         _player.GetComponent<PlayerMovement>().Unfreeze();
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            var viewID = (player.ActorNumber * 1000) + 1;
+            _playersList.Add(PhotonNetwork.GetPhotonView(viewID));
+        }
+        
+        _isGameStart = true;
+    }
+
+    private void RemovePlayer(PhotonView playerView)
+    {
+        _playersList.Remove(playerView);
+        virtualCamera.GetComponent<CameraHandler>().FindNewTarget();
+    }
+
+    private void HandleRace()
+    {
+        if (GetPlayersCount() == 0)
+        {
+            DisplayLose();
+        }
+        else if (GetPlayersCount() == winHandler.Winners.Count)
+        {
+            DisplayWin();
+        }
     }
     
-    private void DisplayGameOver()
+    private void DisplayLose()
     {
         loseUICanvas.gameObject.SetActive(true);
+    }
+
+    private void DisplayWin()
+    {
+        winUICanvas.gameObject.SetActive(true);
+    }
+
+    private int GetPlayersCount()
+    {
+        int index = 0;
+        foreach (var player in _playersList)
+        {
+            if (player != null)
+            {
+                index++;
+            }
+        }
+
+        return index;
     }
 }
